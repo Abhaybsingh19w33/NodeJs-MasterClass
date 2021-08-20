44f9ade88bfb930ee396c9a556534e83a5945de9
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-08-20 09:03:57 +0530

    Routing Requests and comments for previous codess

--- Section 3/index.js
+++ Section 3/index.js
@@ -1,31 +1,35 @@
 // dependencies
 const http = require('http');
 const url = require('url');
-// const StringDecoder = require('string_decoder').StringDecoder;
-const { StringDecoder } = require('string_decoder');
+const StringDecoder = require('string_decoder').StringDecoder;
+// const { StringDecoder } = require('string_decoder');
 
 // server should respond to all request with a string
 const server = http.createServer(function (req, res) {
 
-    // get the url and parse it
-    // true means parse the query string, it will call the query string module 
+    // step - 1 get the url and parse it
+    // true means parse the query string, as it will call the query string module
+    // using 2 module at once
     var parseUrl = url.parse(req.url, true);
 
-    // get the path from url
+    // step - 2 get the path from url
+    // pareUrl contains many values
+    // one of which is pathname and it is untrimmed 
     var path = parseUrl.pathname;
     // trimming the first and last slashes from both sides
     var trimmedPath = path.replace(/\/+|\/+$/g, '')
 
-    // get the query string as an object
+    // step - 2.0 get the query string as an object
+    // all data which is sent throough url will be stored in keys and values pair
     var queryStringObject = parseUrl.query;
 
-    // get the HTTP method
+    // step - 2.1 get the HTTP method like get post, etc
     var method = req.method.toLowerCase();
 
-    // get the headers as an object
+    // step - 2.2 get the headers as an object from req
     var headers = req.headers;
 
-    // get the payload, if there is any
+    // step - 2.3 get the payload, if there is any
     var decoder = new StringDecoder('utf-8');
     var buffer = '';
     // data event, this callback function will be called
@@ -38,21 +42,78 @@ const server = http.createServer(function (req, res) {
         // append the buffer with decoder end
         buffer += decoder.end();
 
-        // when requestt is finished
+        // step - 5.3 Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
+        // here a function varibale is stored here, not a value based variable 
+        var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-        // send the res
-        res.end('hello world\n');
+        // Construct the data object to send to the handler
+        var data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        };
 
-        // log the request
-        // console.log('Request received on path: ' + trimmedPath + ' with method: ' + method + ' and with these query string parameters: ', queryStringObject);
-        console.log('Request received with these payload: ', buffer);
+        // Route the request to the handler specified in the router
+        // chosenHandler had stored handler function so we can pass parameter to that function through chosen handler
+        chosenHandler(data, function (statusCode, payload) {
+
+            // Use the status code returned from the handler, or set the default status code to 200
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+            // Use the payload returned from the handler, or set the default payload to an empty object
+            payload = typeof (payload) == 'object' ? payload : {};
+
+            // Convert the payload to a string
+            var payloadString = JSON.stringify(payload);
+
+            // Return the response
+            // when request is finished
+            // writing the statusCode to the header
+            res.writeHead(statusCode);
+            // closing the payload string
+            res.end(payloadString);
+
+            // log the request path
+            console.log("Returning this response: ", statusCode, payloadString);
+        });
     });
-
-
 });
 
-// start the server, and have it listen on port 3000
+// step - 0 start the server, and have it listen on port 3000
 // when it done listening it will run this function
 server.listen(3000, function () {
     console.log("The server is listening to port 3000 ");
 })
+
+// Step - 5 Define all the handlers
+var handlers = {};
+
+// step - 5.2 Sample handler
+// data contains every bit of data which is passed from browser
+// callback is a function which will be called when the handler had done with their request
+handlers.sample = function (data, callback) {
+    // callback a http status code, and a pyload object
+
+    // 406 Not Acceptable client error response code 
+    // indicates that the server cannot produce a 
+    // response matching the list of acceptable values 
+    // defined in the request's proactive content 
+    // negotiation headers, and that the server is 
+    // unwilling to supply a default representation
+
+    // this payload key value pair is passed
+    callback(406, { 'name': 'sample handler' });
+};
+
+// Not found handler
+handlers.notFound = function (data, callback) {
+    callback(404);
+};
+
+// step - 5.1 Define the request router
+// here when sample passed in url the handler.sample will be called
+var router = {
+    'sample': handlers.sample
+};
